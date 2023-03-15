a4feb3257813b9c600b8805672a9e0a149935fc4
TerehovAleksei <375295565021@yandex.ru>
2023-03-15 17:33:42 +0300

    разработка раздела истории

--- src/pages/HistoryPage/components/GeneralCard.tsx
+++ src/pages/HistoryPage/components/GeneralCard.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {Card, Descriptions, Space} from "antd";
+import {IDay} from "../../../models/IDay";
 
-const GeneralCard = () => {
+type GeneralCardProps = {
+    days: IDay[];
+}
+
+const GeneralCard = ({days}: GeneralCardProps) => {
+
+    const totalCount = useMemo(() => Object.values(days).reduce((a, b) => a + b.count, 0), [days]);
+
     return (
         <Card title="Статистика" bordered={false}>
             <Space direction="vertical" style={{display: 'flex'}}>
                 <Descriptions size="small" column={1}>
-                    <Descriptions.Item label="...">...</Descriptions.Item>
+                    <Descriptions.Item label="Всего доставок">{totalCount}</Descriptions.Item>
+                    <Descriptions.Item label="Всего рабочих дней">{days.length}</Descriptions.Item>
                 </Descriptions>
             </Space>
         </Card>
--- src/pages/HistoryPage/index.tsx
+++ src/pages/HistoryPage/index.tsx
@@ -1,9 +1,34 @@
-import React, {useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import GeneralCard from "./components/GeneralCard";
 import {Card, Form, Select, Space} from "antd";
 import FilterCard from "./components/FilterCard";
+import {SettingsContext} from "../../providers/SettingsProvider";
+import {DefaultOptionType} from 'antd/es/select';
+import {getDays} from "../../firebase/historyApi";
+import {AuthContext} from "../../providers/AuthProvider";
+import {IDay} from "../../models/IDay";
+
+const emptySelector: DefaultOptionType[] = [{label: "Все", value: 'all'}];
 
 const HistoryPage = () => {
+
+    const settings = useContext(SettingsContext);
+    const user = useContext(AuthContext);
+
+    const [selector, setSelector] = useState(emptySelector);
+    const [days, setDays] = useState<IDay[] | null>(null);
+
+    useEffect(() => {
+        if (user) {
+            getDays(user.uid).then(result => setDays(result));
+        }
+    }, []);
+
+    useEffect(() => {
+        if (settings) {
+            setSelector(emptySelector.concat(settings.templates.map(t => ({label: t.name, value: t.id}))));
+        }
+    }, [settings]);
 
     return (
         <Space direction="vertical" style={{display: 'flex'}}>
@@ -14,13 +39,14 @@ const HistoryPage = () => {
                         <Select
                             size="large"
                             onChange={e => console.log(e)}
-                            options={Array(5).fill('item', 0, 5).map((t, i) => ({value: i, label: `${t} ${i}`}))}
+                            options={selector}
+                            defaultValue={selector[0].value}
                         />
                     </Form.Item>
                 </Form>
             </Card>
-            <GeneralCard/>
-            <FilterCard/>
+            <GeneralCard days={days ?? []}/>
+            <FilterCard days={days ?? []}/>
         </Space>
     );
 };
